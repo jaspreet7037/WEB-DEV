164249436079c3bb9a2b2dd72734711c5b3e4c7e
jaspreet singh <jaspreetsinghv077@gmail.com>
2022-11-12 22:59:30 +0530

    files added

---
+++ html9.js
@@ -0,0 +1,8 @@
+function display()
+{
+    alert("Do you want to display?");
+}
+var a=document.getElementById("para1").innerHTML;
+document.write(a)
+var b=document.getElementById("para2").innerHTML;
+document.write(b)
