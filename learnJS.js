c1b7575cb04bd97f27f37faf4fb69f633dc4c680
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2023-06-21 10:31:19 +0530

    learnJs file updated

--- learnJS.js
+++ learnJS.js
@@ -7,3 +7,14 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 }
 
 console.log(wordBlanks("dog", "big", "ran", "quickly"));
+
+function nextInLine(arr, item) {
+    arr.push(item);
+    return arr.shift();
+}
+
+var testArr = [1,2,3,4,5];
+console.log("before:" + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("after:" + JSON.stringify(testArr));
+
