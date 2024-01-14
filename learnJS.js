fea0ea58704e9297ccc525a306a9b1f3a32b4e6f
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2024-01-14 11:22:04 +0530

    Rock Scissor Paper Project using Javascript

--- learnJS.js
+++ learnJS.js
@@ -53,3 +53,45 @@ var myStorage = {
 };
     var gloveBoxContents = myStorage.car.inside["glove box"]; 
     console.log(gloveBoxContents,"\n");
+
+var collection = {
+    "12548": {
+        "album": "isLippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock ",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+        "1999",
+        "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": [ ]
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+};
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value) {
+    if (value == "") {
+        delete collection[id][prop];
+    } else if (prop == "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    } else {
+        collection[id][prop] = value;
+    }
+    return collection;
+}
+updateRecords(2468, "tracks", "test");
+console.log(updateRecords(5439, "artist", "ABBA"));
